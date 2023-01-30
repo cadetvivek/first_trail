218d85b0f26fb40f529a43f6b2d7e04604dbc3f0
vivek <106858243+cadetvivek@users.noreply.github.com>
2023-01-30 14:53:33 +0530

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,6 @@
+var name = "vivek";
+var father_name = "sunaheri lal";
+var mother_name = "shanti devi";
+console.log(name)
+console.log(father_name)
+console.log(mother_name)
---
+++ problem3.js
@@ -0,0 +1,5 @@
+var name = "vivek";
+var age = 22;
+console.log(name,age)
+console.log(typeof(name))
+console.log(typeof(age))
